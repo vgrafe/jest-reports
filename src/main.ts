bcdf13bf1a3e702c06878301d39719ec314072c9
Vincent Grafe <vincent.grafe@gmail.com>
2022-06-29 22:22:25 -0400

    adds stuff

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as glob from "@actions/glob";
 import { exec } from "@actions/exec";
 import { compareAndPost } from "./compareAndPost";
 import { summariesToTable } from "./summaryToTable";
+import { summary1, summary2 } from "../mock/json-summary";
 
 const getCoverageAtBranch = async (sha: string, fileName: string) => {
   await exec(`git fetch`, undefined, {
@@ -96,69 +97,7 @@ const run = async () => {
 };
 
 const test = () => {
-  const sum1 = {
-    total: {
-      lines: { total: 15, covered: 15, skipped: 0, pct: 100 },
-      statements: { total: 19, covered: 19, skipped: 0, pct: 100 },
-      functions: { total: 6, covered: 6, skipped: 0, pct: 100 },
-      branches: { total: 6, covered: 4, skipped: 0, pct: 66.66 },
-      branchesTrue: { total: 0, covered: 0, skipped: 0, pct: 100 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/components/TextSwitch.tsx": {
-      lines: { total: 2, covered: 2, skipped: 0, pct: 100 },
-      functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
-      statements: { total: 3, covered: 3, skipped: 0, pct: 100 },
-      branches: { total: 2, covered: 1, skipped: 0, pct: 50 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/components/TextSwitch2.tsx": {
-      lines: { total: 2, covered: 2, skipped: 0, pct: 100 },
-      functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
-      statements: { total: 3, covered: 3, skipped: 0, pct: 100 },
-      branches: { total: 2, covered: 1, skipped: 0, pct: 50 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/components/TextSwitch3.tsx": {
-      lines: { total: 5, covered: 5, skipped: 0, pct: 100 },
-      functions: { total: 3, covered: 3, skipped: 0, pct: 100 },
-      statements: { total: 7, covered: 7, skipped: 0, pct: 100 },
-      branches: { total: 2, covered: 2, skipped: 0, pct: 100 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/pages/index.tsx": {
-      lines: { total: 6, covered: 6, skipped: 0, pct: 100 },
-      functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
-      statements: { total: 6, covered: 6, skipped: 0, pct: 100 },
-      branches: { total: 0, covered: 0, skipped: 0, pct: 100 },
-    },
-  };
-
-  const sum2 = {
-    total: {
-      lines: { total: 15, covered: 15, skipped: 0, pct: 100 },
-      statements: { total: 19, covered: 19, skipped: 0, pct: 100 },
-      functions: { total: 6, covered: 6, skipped: 0, pct: 100 },
-      branches: { total: 6, covered: 4, skipped: 0, pct: 66.66 },
-      branchesTrue: { total: 0, covered: 0, skipped: 0, pct: 100 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/components/TextSwitch.tsx": {
-      lines: { total: 2, covered: 2, skipped: 0, pct: 100 },
-      functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
-      statements: { total: 3, covered: 3, skipped: 0, pct: 100 },
-      branches: { total: 2, covered: 1, skipped: 0, pct: 50 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/components/TextSwitch2.tsx": {
-      lines: { total: 2, covered: 2, skipped: 0, pct: 100 },
-      functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
-      statements: { total: 3, covered: 3, skipped: 0, pct: 100 },
-      branches: { total: 2, covered: 1, skipped: 0, pct: 50 },
-    },
-    "/Users/vgrafe/Code/with-jest-app/pages/index.tsx": {
-      lines: { total: 6, covered: 6, skipped: 0, pct: 100 },
-      functions: { total: 1, covered: 1, skipped: 0, pct: 100 },
-      statements: { total: 6, covered: 6, skipped: 0, pct: 100 },
-      branches: { total: 0, covered: 0, skipped: 0, pct: 100 },
-    },
-  };
-
-  const a = summariesToTable(sum1, sum2);
+  const a = summariesToTable(summary1, summary2);
 
   console.log("summaryTable");
   console.log(a.summaryTable);
